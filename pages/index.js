b37e460765feaaadf21ea438e6c941d198817bd0
onlyhouse1980 <56930138+onlyhouse1980@users.noreply.github.com>
2022-03-26 09:30:05 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -47,14 +47,16 @@ export default function Home({ data }) {
         <title> NextJS Image Gallery</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Box overflow="hidden" bg="purple.100" minH="100vh">
+      <Box overflow="hidden" bg="gray.700" minH="100vh">
         <Container>
           <Text
-            color="pink.800"
+            color="blue.100"
             fontWeight="semibold"
             mb="1rem"
             textAlign="center"
-            textDecoration="underline"
+            textDecoration="none"
+            marginBottom="50px"
+            marginTop="50px"
             fontSize={["4xl", "4xl", "5xl", "5xl"]}
           >
             NextJS Image Gallery
@@ -73,7 +75,7 @@ export default function Home({ data }) {
                   <IconButton
                     aria-label="Search"
                     icon={<SearchIcon />}
-                    bg="pink.400"
+                    bg="blue.400"
                     color="white"
                     onClick={handleSubmit}
                   />
@@ -87,7 +89,7 @@ export default function Home({ data }) {
             <WrapItem
               key={pic.id}
               boxShadow="base"
-              rounded="20px"
+              rounded="10px"
               overflow="hidden"
               bg="white"
               lineHeight="0"
@@ -97,8 +99,8 @@ export default function Home({ data }) {
                 <a>
                   <Image
                     src={pic.src.portrait}
-                    height={600}
-                    width={400}
+                    height={400}
+                    width={300}
                     alt={pic.url}
                   />
                 </a>
